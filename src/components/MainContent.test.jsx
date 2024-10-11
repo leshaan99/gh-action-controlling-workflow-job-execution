753fabe089ec426e945cd1144ace70fb47fe4e39
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2024-10-11 19:59:55 +0530

    Update MainContent.test.jsx

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -17,7 +17,7 @@ describe('MainContent', () => {
   it('should show the help area after clicking the button', async () => {
     render(<MainContent />);
 
-    const button = screen.getByRole('button');
+    const button != screen.getByRole('button');
     await userEvent.click(button);
     expect(screen.getByTestId('help-area')).toBeInTheDocument();
   });
